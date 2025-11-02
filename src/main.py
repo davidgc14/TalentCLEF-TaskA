ba867a3e7d892ae5a6dbdc72ccdc6c4d230c31d4
David <curbelo.gc@gmail.com>
2025-11-02 20:32:28 +0000

    feat: Improve it for multilanguage case

--- src/main.py
+++ src/main.py
@@ -6,16 +6,37 @@ import time
 import os
 
 
-def monolingual_evaluation(lang, source='validation', model_name="all-MiniLM-L6-v2", nickname="default", device='cpu'):
+lang_dict = {
+    'en': 'english',
+    'es': 'spanish',
+    'de': 'german',
+    'zh': 'chinese',
+}
 
-    today = time.strftime("%Y-%m-%d")
-    runtime = time.strftime("%Y%m%d_%H%M%S")
+today = time.strftime("%Y-%m-%d")
 
-    project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    data_dir = os.path.join(project_dir, 'data', source, lang)
-    output_dir = os.path.join(project_dir, 'src', 'output', today)
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+date_dir = os.path.join(project_dir, 'src', 'output', today)
+os.makedirs(date_dir, exist_ok=True)
 
+# comprobar si la ultima ejecucion esta vacia
+
+exec_dirs = sorted([d for d in os.listdir(date_dir) if d.isdigit() and len(d) == 3])
+
+if exec_dirs and not os.listdir(os.path.join(date_dir, exec_dirs[-1])):
+    output_dir = os.path.join(date_dir, exec_dirs[-1])
+else:
+    next_id = int(exec_dirs[-1]) + 1 if exec_dirs else 1
+    output_dir = os.path.join(date_dir, f"{next_id:03d}")
     os.makedirs(output_dir, exist_ok=True)
+
+
+
+def monolingual_evaluation(lang, model, source='validation', nickname="default", device='cpu'):
+
+
+    data_dir = os.path.join(project_dir, 'data', source, lang_dict[lang])
+
 
     queries_path = os.path.join(data_dir, "queries")
     corpus_elements_path = os.path.join(data_dir, "corpus_elements")
@@ -30,15 +51,15 @@ def monolingual_evaluation(lang, source='validation', model_name="all-MiniLM-L6-
     queries_ids = queries.q_id.to_list()
     queries_texts = queries.jobtitle.to_list()
 
-    # map_queries = dict(zip(queries_ids,queries_texts))
 
     corpus_ids = corpus_elements.c_id.to_list()
     corpus_texts = corpus_elements.jobtitle.to_list()
-    # map_corpus = dict(zip(corpus_ids,corpus_texts))
 
-    print('Loading model:', model_name, 'on device:', device)
 
-    model = SentenceTransformer(model_name, device=device)
+    model_name = model[0].auto_model.config._name_or_path 
+    model_name = model_name.split("/")[-1]
+    print('Encoding data:', model_name, 'on device:', device)
+
     query_embeddings = model.encode(queries_texts, convert_to_tensor=True, show_progress_bar=True)
     corpus_embeddings = model.encode(corpus_texts, convert_to_tensor=True, show_progress_bar=True)
 
@@ -57,7 +78,7 @@ def monolingual_evaluation(lang, source='validation', model_name="all-MiniLM-L6-
             results.append(f"{str(q_id)} Q0 {str(doc_id)} {rank+1} {score:.4f} {model_name}")
 
 
-    run_file = os.path.join(output_dir, f"evaluation_{lang}_{nickname}_{runtime}.trec")
+    run_file = os.path.join(output_dir, f"run_{lang}-{lang}_{model_name}.trec")
 
     print('Writing results to:', os.path.basename(run_file))
 
@@ -67,7 +88,7 @@ def monolingual_evaluation(lang, source='validation', model_name="all-MiniLM-L6-
 
     print('Evaluating with evaluation_file.py...')
 
-    command = ["python", "evaluation_file.py", "--qrels", qrels_path, "--run", run_file]
+    command = ["python", os.path.join(project_dir, "src", "evaluation_file.py"), "--qrels", qrels_path, "--run", run_file]
 
     result = subprocess.run(command, capture_output=True, text=True)
 
@@ -76,17 +97,44 @@ def monolingual_evaluation(lang, source='validation', model_name="all-MiniLM-L6-
     print(result.stdout)
 
 
-    print('Saving evaluation results...')
-
-    with open(os.path.join(output_dir, f"results_{lang}_{runtime}.txt"), "w", encoding="utf-8") as f:
-        f.write(f"Model name: {model_name}\n\n")
-        f.write(f"Model alias: {nickname}\n\n")
-        f.write("\n".join(result.stdout.splitlines()[-7:]))
-
 
     print('Evaluation completed for language:', lang)
     return result.stdout
 
 
+
+
+
+def all_lang_evaluation(model_name="all-MiniLM-L6-v2", nickname="default", device='cpu'):
+
+    model = SentenceTransformer(model_name, device=device)
+
+    results = {}
+    for lang in lang_dict.keys():
+        print(f"Starting evaluation for language: {lang}")
+        results[lang] = monolingual_evaluation(lang=lang, model=model, nickname=nickname, device=device)
+
+    print("All evaluations completed. Saving all evaluation results...")
+
+    with open(os.path.join(output_dir, f"results.txt"), "w", encoding="utf-8") as f:
+        f.write("=== Evaluation Results for multilingual ===\n")
+        f.write(f"Model name: {model_name}\n\n")
+        f.write(f"Model alias: {nickname}\n\n")
+        for lang in results:
+            f.write(f"=== Results for language: {lang} ===\n")
+            f.write(results[lang])
+            f.write("\n\n")
+
+
+
+
+def main():
+    # Example usage
+    all_lang_evaluation(model_name="all-MiniLM-L6-v2", nickname="MiniLM_v2", device='cpu')
+
+
+
+
+
 if __name__ == "__main__":
-    monolingual_evaluation(lang='english')
+    main()
